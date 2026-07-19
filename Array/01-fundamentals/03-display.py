5a138b558e4fae46005af74c0df9a7b9be48d128
Gagan Sahu <gagansahu55428@gmail.com>
2026-07-19 14:30:29 +0530

    feat : displaying elements 	new file:   Array/01-fundamentals/03-display.py

---
+++ Array/01-fundamentals/03-display.py
@@ -0,0 +1,23 @@
+array = [1,2, 3, 4, 5, 6, 7]
+
+# display complete array
+print(array)
+
+# display elements one by one
+for elements in array:
+    print(elements)
+
+# to display with index
+for index in range(len(array)):
+    print("Index = ", index, "Value = ", array[index] )
+
+
+
+'''
+array = [1, 2, 3, 4, 5, 6, 7]
+
+index = 0
+for elements in array:
+    print("Index =", index, "Value =", elements)
+    index += 1
+'''
